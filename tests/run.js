e1e0cbd26c1b4737e5450569da91711c118612b0
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-11-12 14:14:12 +0200

    *

--- tests/run.js
+++ tests/run.js
@@ -1,21 +1,32 @@
-import { Parser } from "../src/parser.js";
-
+import assert from 'node:assert';
+import { Parser } from '../src/parser.js';
+import { readdirSync } from 'node:fs';
 
 const parser = new Parser();
 
+const files = readdirSync(import.meta.url.slice(7, -6)).filter((f) => f.endsWith('test.js'));
+const imports = files.map(async (curr) => await import(`./${curr}`));
 
-const program = `   
-/**
- * 
- * 
- *  String
- **/
+const exec = () => {
+  const program = `
+  50;
+  "hello";
+  `;
+  const ast = parser.parse(program);
+  console.log(JSON.stringify(ast, null, 2));
+};
 
-100`;
+function test(program, expected) {
+  const ast = parser.parse(program);
+  assert.deepEqual(ast, expected);
+}
 
+const testRun = async () => {
+  for await (const iterator of imports) {
+    iterator.default(test);
+  }
+  console.log('All source tests passed!');
+};
 
-
-const ast = parser.parse(program);
-
-
-console.log(JSON.stringify(ast, null, 2))
+testRun();
+exec();
