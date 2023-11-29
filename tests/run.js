231383834660750cf007176446a005fc4d827f91
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-11-29 11:23:37 +0200

    *

--- tests/run.js
+++ tests/run.js
@@ -9,7 +9,7 @@ const imports = files.map(async (curr) => await import(`./${curr}`));
 
 const exec = () => {
   const program = `
-   w =  (2+3) - 10;
+   x =   10;
   `;
   const ast = parser.parse(program);
   console.log(JSON.stringify(ast, null, 2));
