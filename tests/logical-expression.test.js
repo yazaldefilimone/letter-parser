816bcaab4432742fd08d3cde42f2eb8804e4a44c
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-12 18:51:27 +0200

    *

---
+++ tests/logical-expression.test.js
@@ -0,0 +1,80 @@
+import { ASTTypes } from '../src/parser.js';
+
+export default (test) => {
+  const LogicalANDExpression = {
+    type: 'Program',
+    body: [
+      {
+        type: ASTTypes.ExpressionStatement,
+        expression: {
+          type: ASTTypes.LogicalAndExpression,
+          operator: '&&',
+          left: {
+            type: ASTTypes.BinaryExpression,
+            operator: '<',
+            left: {
+              type: ASTTypes.Identifier,
+              name: 'x',
+            },
+            right: {
+              type: ASTTypes.NumericLiteral,
+              value: '1',
+            },
+          },
+          right: {
+            type: ASTTypes.BinaryExpression,
+            operator: '>',
+            left: {
+              type: ASTTypes.Identifier,
+              name: 'x',
+            },
+            right: {
+              type: ASTTypes.NumericLiteral,
+              value: '-5',
+            },
+          },
+        },
+      },
+    ],
+  };
+  test('x < 1 && x > -5', LogicalANDExpression);
+
+  const LogicalORExpression = {
+    type: 'Program',
+    body: [
+      {
+        type: ASTTypes.ExpressionStatement,
+        expression: {
+          type: ASTTypes.LogicalOrExpression,
+          operator: '||',
+          left: {
+            type: ASTTypes.BinaryExpression,
+            operator: '>',
+            left: {
+              type: ASTTypes.Identifier,
+              name: 'x',
+            },
+            right: {
+              type: ASTTypes.NumericLiteral,
+              value: '1',
+            },
+          },
+          right: {
+            type: ASTTypes.BinaryExpression,
+            operator: '<',
+            left: {
+              type: ASTTypes.Identifier,
+              name: 'x',
+            },
+            right: {
+              type: ASTTypes.NumericLiteral,
+              value: '-5',
+            },
+          },
+        },
+      },
+    ],
+  };
+
+  test('x > 1 || x < -5', LogicalORExpression);
+};
