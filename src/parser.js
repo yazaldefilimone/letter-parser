78aeff8d7a07a0fc89b38e018883f4ca3f70c1d3
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-12 15:52:41 +0200

    *

--- src/parser.js
+++ src/parser.js
@@ -16,6 +16,7 @@ export const ASTTypes = {
   VariableDeclarator: `VariableDeclarator`,
   BinaryExpression: `BinaryExpression`,
   BooleanLiteral: `BooleanLiteral`,
+  NullLiteral: `NullLiteral`,
 };
 
 export class Parser {
@@ -79,6 +80,11 @@ export class Parser {
         return this.ExpressionStatement();
     }
   }
+  /*
+    * IfStatement
+    * : IF ParenthesizedExpression Statement ElseStatement
+    * ;
+    /
   IfStatement() {
     const ifKeyword = this._eat(tokensEnum.IF);
     const test = this.ParenthesizedExpression();
@@ -91,6 +97,12 @@ export class Parser {
       alternate,
     };
   }
+  /*
+   * ElseStatement
+   * : ELSE Statement
+   * | IfStatement
+   * ;
+   */
   ElseStatement() {
     if (this._lookAhead.type !== tokensEnum.ELSE) {
       return null;
@@ -99,6 +111,11 @@ export class Parser {
     const statement = this.Statement();
     return statement;
   }
+  /*
+   * VariableStatement
+   * : LET VariableDeclarationList SEMICOLON
+   * ;
+   */
   VariableStatement() {
     const letKeyword = this._eat(tokensEnum.LET);
     const declarations = this.VariableDeclarationList();
@@ -108,6 +125,12 @@ export class Parser {
       declarations,
     };
   }
+  /*
+   * VariableDeclarationList
+   * : VariableDeclarationList COMMA VariableDeclaration
+   * | VariableDeclaration
+   * ;
+   */
   VariableDeclarationList() {
     const declarations = [this.VariableDeclaration()];
     while (this._lookAhead.type === tokensEnum.COMMA) {
@@ -116,7 +139,12 @@ export class Parser {
     }
     return declarations;
   }
-
+  /*
+   * VariableDeclaration
+   * : Identifier
+   * | Identifier SIMPLE_ASSIGNMENT AssignmentExpression
+   * ;
+   */
   VariableDeclaration() {
     const id = this.Identifier();
     let init = null;
@@ -132,11 +160,21 @@ export class Parser {
       init,
     };
   }
-
+  /*
+   * VariableInitializer
+   * : SIMPLE_ASSIGNMENT AssignmentExpression
+   * ;
+   */
   VariableInitializer() {
     this._eat(tokensEnum.SIMPLE_ASSIGNMENT);
     return this.AssignmentExpression();
   }
+  /*
+   * AssignmentExpression
+   * : LeftHandSideExpression AssignmentOperator AssignmentExpression
+   * | EqualityExpression
+   * ;
+   */
   AssignmentExpression() {
     const leftToken = this.EqualityExpression();
     if (!this._isAssignmentOperator(this._lookAhead.type)) {
@@ -153,10 +191,22 @@ export class Parser {
       right: rightToken,
     };
   }
-  // relational operators:  <, >, <=, >=
+  /*
+   * EqualityExpression
+   * : RelationalExpression
+   * | EqualityExpression EQUALITY_OPERATOR RelationalExpression ( <, >, <=, >=)
+   * ;
+   */
   RelationalExpression() {
     return this._BinaryExpression('AdditiveExpression', tokensEnum.RELATIONAL_OPERATOR);
   }
+  /*
+   * AssignmentOperator
+   * : SIMPLE_ASSIGNMENT
+   * | COMPLEX_ASSIGNMENT
+   * | RELATIONAL_OPERATOR
+   * ;
+   */
   AssignmentOperator() {
     if (this._lookAhead.type === tokensEnum.COMPLEX_ASSIGNMENT) {
       return this._eat(tokensEnum.COMPLEX_ASSIGNMENT);
@@ -166,10 +216,20 @@ export class Parser {
     }
     return this._eat(tokensEnum.SIMPLE_ASSIGNMENT);
   }
+  /*
+   * LeftHandSideExpression
+   * : Identifier
+   * ;
+   */
 
   LeftHandSideExpression() {
     return this.Identifier();
   }
+  /*
+   * ExpressionStatement
+   * : Expression SEMICOLON
+   * ;
+   */
   ExpressionStatement() {
     const expression = this.Expression();
     this._eat(tokensEnum.SEMICOLON);
@@ -178,7 +238,11 @@ export class Parser {
       expression,
     };
   }
-
+  /*
+   * BlockStatement
+   * : LEFT_BRACE StatementList RIGHT_BRACE
+   * ;
+   */
   BlockStatement() {
     this._eat(tokensEnum.LEFT_BRACE);
     let body;
@@ -194,22 +258,49 @@ export class Parser {
       body,
     };
   }
+  /*
+   * Expression
+   * : AssignmentExpression
+   * ;
+   */
   Expression() {
     return this.AssignmentExpression();
   }
+  /*
+    * EmptyStatement
+    * : SEMICOLON
+    * ;
   EmptyStatement() {
     this._eat(tokensEnum.SEMICOLON);
     return {
       type: ASTTypes.EmptyStatement,
     };
   }
+  /*
+   * EqualityExpression
+   * : RelationalExpression
+   * | EqualityExpression EQUALITY_OPERATOR RelationalExpression
+   * ;
+   * */
+
   EqualityExpression() {
     return this._BinaryExpression('RelationalExpression', tokensEnum.EQUALITY_OPERATOR);
   }
+  /*
+   * AdditiveExpression
+   * : MultiplicativeExpression
+   * | AdditiveExpression ADDITIVE_OPERATOR MultiplicativeExpression
+   * ;
+   */
   AdditiveExpression() {
     return this._BinaryExpression('MultiplicativeExpression', tokensEnum.ADDITIVE_OPERATOR);
   }
-
+  /*
+   * MultiplicativeExpression
+   * : PrimaryExpression
+   * | MultiplicativeExpression MULTIPLICATIVE_OPERATOR PrimaryExpression
+   * ;
+   */
   MultiplicativeExpression() {
     return this._BinaryExpression('PrimaryExpression', tokensEnum.MULTIPLICATIVE_OPERATOR);
   }
@@ -218,6 +309,29 @@ export class Parser {
    * Literal
    * :NumericLiteral
    * |StringLiteral
+   * ;
+   */
+  Literal() {
+    switch (this._lookAhead.type) {
+      case tokensEnum.NUMBER:
+        return this.NumericLiteral();
+      case tokensEnum.STRING:
+        return this.StringLiteral();
+      case tokensEnum.NULL:
+        return this.NullLiteral();
+      case tokensEnum.TRUE:
+        return this.BooleanLiteral(true);
+      case tokensEnum.FALSE:
+        return this.BooleanLiteral(false);
+      default:
+        throw new SyntaxError('Literal: unexpected literal production, receive ' + this._lookAhead.type);
+    }
+  }
+  /*
+   * PrimaryExpression
+   * : Literal
+   * | Identifier
+   * | ParenthesizedExpression
    * ;
    */
   PrimaryExpression() {
@@ -229,21 +343,43 @@ export class Parser {
     }
     return this.LeftHandSideExpression();
   }
+  /*
+   * ParenthesizedExpression
+   * : LEFT_PARENT Expression RIGHT_PARENT
+   * ;
+   */
   ParenthesizedExpression() {
     this._eat(tokensEnum.LEFT_PARENT);
     const expression = this.Expression();
     this._eat(tokensEnum.RIGHT_PARENT);
     return expression;
   }
-  Literal() {
-    switch (this._lookAhead.type) {
-      case tokensEnum.NUMBER:
-        return this.NumericLiteral();
-      case tokensEnum.STRING:
-        return this.StringLiteral();
-      default:
-        throw new SyntaxError('Literal: unexpected literal production, receive ' + this._lookAhead.type);
-    }
+
+  /*
+   * NullLiteral
+   *  :NULL
+   *   ;
+   */
+  NullLiteral() {
+    const token = this._eat(tokensEnum.NULL);
+    return {
+      type: ASTTypes.NullLiteral,
+      value: null,
+    };
+  }
+  /*
+   * BooleanLiteral
+   *  :TRUE
+   * |FALSE
+   * ;
+   * */
+  BooleanLiteral(value) {
+    const tokenEnum = value ? tokensEnum.TRUE : tokensEnum.FALSE;
+    const booleanKeyword = this._eat(tokensEnum[tokenEnum]);
+    return {
+      type: ASTTypes.BooleanLiteral,
+      value,
+    };
   }
 
   /**
@@ -258,6 +394,11 @@ export class Parser {
       value: token.value,
     };
   }
+  /*
+   * StringLiteral
+   *  :STRING
+   * ;
+   */
   StringLiteral() {
     const token = this._eat(tokensEnum.STRING);
     return {
@@ -265,7 +406,11 @@ export class Parser {
       value: token.value.slice(1, -1),
     };
   }
-
+  /*
+   * Identifier
+   *  :IDENTIFIER
+   * ;
+   */
   Identifier() {
     const token = this._eat(tokensEnum.IDENTIFIER);
     return {
@@ -273,7 +418,6 @@ export class Parser {
       name: token.value,
     };
   }
-
   _eat(type) {
     const token = this._lookAhead;
     if (token === null) {
@@ -297,7 +441,16 @@ export class Parser {
     throw SyntaxError(`Left side of assignment must be a variable`);
   }
   _isLiteral(tokenType) {
-    return tokenType === tokensEnum.NUMBER || tokenType === tokensEnum.STRING;
+    switch (tokenType) {
+      case tokensEnum.NUMBER:
+      case tokensEnum.STRING:
+      case tokensEnum.NULL:
+      case tokensEnum.TRUE:
+      case tokensEnum.FALSE:
+        return true;
+      default:
+        return false;
+    }
   }
   _BinaryExpression(builder, tokenEnum) {
     let left = this[builder]();
