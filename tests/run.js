c236f6d2fae7af2f00f01d7df36ce3e8e1bb8fff
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-17 16:02:25 +0200

    *

--- tests/run.js
+++ tests/run.js
@@ -9,8 +9,30 @@ const imports = files.map(async (curr) => await import(`./${curr}`));
 
 const exec = () => {
   const program = `
-   str.length;
-    str[0];
+   class Point {
+    def constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    def calc() {
+      return this.x + this.y;
+    }
+
+   }
+
+   class Point3D extends Point {
+    def constructor(x, y, z) {
+      super(x, y);
+      this.z = z;
+    }
+
+    def calc() {
+      return super.calc() + this.z;
+    }
+   }
+    let p3d = new Point3D(10, 20, 30);
+    p3d.calc();
   `;
   const ast = parser.parse(program);
   console.log(JSON.stringify(ast, null, 2));
@@ -28,5 +50,5 @@ const testRun = async () => {
 };
 
 exec();
-testRun();
+// testRun();
 console.log('All source tests passed!');
